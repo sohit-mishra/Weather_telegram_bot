da4bce98eb2b5dca5687b2897a66451d431ef6b8
Sohit Mishra <7836020160s@gmail.com>
2025-01-17 19:43:07 +0530

    Complete

--- bot.js
+++ bot.js
@@ -1,16 +1,19 @@
+const express = require('express');
+const app = express();
 require('dotenv').config();
 const { Telegraf } = require('telegraf')
 const axios = require('axios');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const PORT = process.env.PORT || 3000;
 
 
-const getWeather = async(city)=>{
-    
+const getWeather = async (city) => {
+
     const apiKey = process.env.WEATHER_API_KEY;
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    
+
     try {
         const response = await axios.get(url);
         const data = response.data;
@@ -19,7 +22,13 @@ const getWeather = async(city)=>{
         const feelsLike = data.main.feels_like;
         const humidity = data.main.humidity;
 
-        return `Weather in ${city}:\nDescription: ${weatherDescription}\nTemperature: ${temperature}°C\nFeels Like: ${feelsLike}°C\nHumidity: ${humidity}%`;
+        return `🌆 Weather in ${city}:
+        
+🌫️ Description: ${weatherDescription}
+🌡️ Temperature: ${temperature}°C
+🌡️ Feels Like: ${feelsLike}°C
+💧 Humidity: ${humidity}%`;
+
     } catch (error) {
         return ('Sorry, I could not retrieve the weather for that location. Please check the city name or try again later.')
     }
@@ -36,3 +45,12 @@ bot.on('text', async (ctx) => {
 });
 
 bot.launch().then(() => console.log('Weather bot is running...'));
+
+
+app.get('', (req, res) => {
+    res.send('Bot is running!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
